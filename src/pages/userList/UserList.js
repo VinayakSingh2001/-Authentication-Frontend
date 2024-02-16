a00b51d28708755f5141a050061d4c2b7e4506ed
VinayakSingh2001 <vinayakthakur843@gmail.com>
2024-02-17 00:46:35 +0530

    added Auth frontend

---
+++ src/pages/userList/UserList.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./UserList.scss";
+import { FaTrashAlt } from "react-icons/fa";
+import PageMenu from "../../components/pageMenu/PageMenu";
+import UserStats from "../../components/userStats/UserStats";
+import Search from "../../components/search/Search";
+import ChangeRole from "../../components/changeRole/ChangeRole";
+
+const UserList = () => {
+  return (
+    <section>
+      <div className="container">
+        <PageMenu />
+        <UserStats />
+        <div className="user-list">
+          <div className="table">
+            <div className="--flex-between">
+              <span>
+                <h3>All Users</h3>
+              </span>
+              <span>
+                <Search />
+              </span>
+            </div>
+            {/* Table */}
+            <table>
+              <thead>
+                <tr>
+                  <th>s/n</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Change Role</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Vinayak</td>
+                  <td>vinayak@test.com</td>
+                  <td>admin</td>
+                  <td>
+                    <ChangeRole />
+                  </td>
+                  <td>
+                    <span>
+                      {" "}
+                      <FaTrashAlt size={20} color="red" />
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default UserList;
